438467db44e9705770a6118ab48f32d7eea5130d
MinikLawa <dominik.pulawa@gmail.com>
2026-01-30 23:11:54 +0100

    update jump state machine

--- src/jump_state_machine.gd
+++ src/jump_state_machine.gd
@@ -10,7 +10,7 @@ func _enter():
 
 func _physics_update(_delta: float) -> void:
 	var direction = Input.get_axis("ui_left", "ui_right")
-	owner.velocity.y += 1500 * _delta
+	owner.velocity.y += 4000 * _delta
 	
 	owner.velocity.x = direction * 200
 	owner.move_and_slide()
