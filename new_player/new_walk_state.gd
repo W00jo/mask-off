1b70de8e37fc3a44087dc015ac7d370edcadde40
rzabka1 <jturovska@gmail.com>
2026-01-31 00:11:13 +0100

    Add NewStateMachine
    Added a new_playerfolder with test player scene and new state machine, to not mess with the original player scripts

---
+++ new_player/new_walk_state.gd
@@ -0,0 +1,35 @@
+class_name NewWalkState
+extends NewState
+
+var speed = 150
+
+func enter():
+	print("entering WALK state...")
+	#owner.get_node("AnimationPlayer").play("walk")
+
+func physics_update(_delta:float):
+	
+	if owner.is_on_floor():
+		var direction = Input.get_axis("ui_left","ui_right")
+		
+		if direction < 0:
+			$"../../Sprite2D".flip_h = true
+		elif direction > 0:
+			$"../../Sprite2D".flip_h = false
+		
+		if direction == 0:
+				state_machine.change_state("newidlestate")
+				return
+				
+		owner.velocity.x = direction * speed
+		owner.move_and_slide()
+	else:
+		state_machine.change_state("newfallstate")
+		return
+	
+
+func handle_input(event:InputEvent):
+	if Input.is_action_just_pressed("ui_accept"):
+		state_machine.change_state("newjumpstate")
+	if Input.is_action_pressed("Shift"):
+		state_machine.change_state("newrunstate")
