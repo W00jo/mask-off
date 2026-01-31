82a0353a827cb3eae7f41d821bc17b15cd9ba6ff
Wujo <w00jcikjakub@gmail.com>
2026-01-31 01:15:17 +0100

    Comment state.gd

--- src/state.gd
+++ src/state.gd
@@ -1,17 +1,28 @@
 @abstract 
 class_name State 
 extends Node
+## A brief description of the class's role and functionality.
+##
+## The description of the script, what it can do,
 
 signal transition(new_state_name: StringName)
 
+## Co się dzieje na wejście do funkcji
+## Np. print wszedł w funkcję
 func _enter() -> void:
 	pass
 
+## Nie używamy
+## Co się dzieje przy wyjściu z funkcji
 func _exit() -> void:
 	pass
 
+## 
+## 
 func _update(_delta: float) -> void:
 	pass
-	
+
+##
+##
 func _physics_update(_delta: float) -> void:
 	pass
