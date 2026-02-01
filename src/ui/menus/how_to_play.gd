a7333a58c5b5eba94cf4545b015ba3c2fb36477e
Wujo <w00jcikjakub@gmail.com>
2026-02-01 15:24:12 +0100

    Pretty UI

--- src/ui/menus/how_to_play.gd
+++ src/ui/menus/how_to_play.gd
@@ -1,10 +1,4 @@
 extends Control
 
-signal start_pressed
-
-
-func _on_start_pressed() -> void:
-	start_pressed.emit()
-
-func _on_button_pressed() -> void:
+func _on_back_pressed() -> void:
 	get_tree().change_scene_to_file("res://src/ui/menus/main_menu.tscn")
