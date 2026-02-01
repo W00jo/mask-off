7493cf671d7480b60c90b5ab526da43e20f44707
Roksana <roksana.lazarska@gmail.com>
2026-02-01 13:54:42 +0100

    pauza

--- src/ui/in_game/pause.gd
+++ src/ui/in_game/pause.gd
@@ -4,3 +4,11 @@ signal resume_pressed
 
 func _on_resume_pressed() -> void:
 	resume_pressed.emit()
+
+
+func _on_menu_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_exit_pressed() -> void:
+	pass # Replace with function body.
